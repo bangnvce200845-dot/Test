fe43c64d628a5abd319b04c1e6f4e18257a1caf0
bangnvce200845-dot <bangnvce200845@gmail.com>
2025-12-02 10:16:09 +0700

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,23 +1,23 @@
 function upDate(previewPic) {
-    // Log to console to check event trigger
-    console.log(previewPic);
+    // Check event is triggering
+    console.log("Event triggered!");
 
-    // Log information about the previewPic (alt and source)
-    console.log("Image alt text: " + previewPic.alt);
-    console.log("Image source: " + previewPic.src);
+    // Print out alt and source
+    console.log("Alt:", previewPic.alt);
+    console.log("Source:", previewPic.src);
 
-    // Change the text of the element with id="image" to the alt text of the previewPic
-    document.getElementById('image').innerHTML = previewPic.alt;
+    // Change text of #image
+    document.getElementById("image").innerText = previewPic.alt;
 
-    // Change the background image of the element with id="image" to the source of the previewPic
-    document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+    // Change background image of #image
+    document.getElementById("image").style.backgroundImage =
+        "url('" + previewPic.src + "')";
 }
 
-// Function to undo the changes when triggered
 function undo() {
-    // Reset the background image of the div with id="image" to the original value
-    document.getElementById('image').style.backgroundImage = "url('')";
+    // Reset background image
+    document.getElementById("image").style.backgroundImage = "url('')";
 
-    // Reset the text of the div with id="image" to the original message
-    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
-}
+    // Reset text
+    document.getElementById("image").innerText =
+        "Hover over an image below to display here.";
